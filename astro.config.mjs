329167b6b4a222da549dbe62c6cdda09d3228354
Franco Androetto <androettofranco@gmail.com>
2026-01-31 11:39:38 -0300

    Update Astro configuration and restructure index page with new layout and components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://androetto.github.io',
+  base: '/santiago-dora/',
+});
