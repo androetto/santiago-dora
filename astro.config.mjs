27547fc42a592dd7a176ba5f5ea14b8c6b353d1d
Franco Androetto <androettofranco@gmail.com>
2026-02-01 00:25:28 -0300

    Update Astro configuration with new site URL and base path; modify Hero component styles for improved typography and visual consistency; replace audio file with updated version.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://androetto.github.io',
-  base: '/santiago-dora/',
+  site: 'https://santiagodora.com',
+  base: '/',
 });
